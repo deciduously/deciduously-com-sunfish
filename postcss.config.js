bcbbc5bc924d0f3763894e03c6eb5560aa6ae1b0
Ben Lovy <ben@deciduously.com>
2020-01-28 21:54:59 -0500

    Style through cv header (#4)
    Docker GH workflow broken, but local docker build and push works fine
    
    * Fix 404
    
    * Working tailwind
    
    * Style through CV header
    
    * Add style prebuild
    
    * Update rust.yml

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ]
-  }
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    purgecss({
+      content: ['./**/*.html']
+    }),
+    require("cssnano")({
+      preset: "default"
+    })
+  ]
+};
--- src/handlers.rs
+++ src/handlers.rs
@@ -12,9 +12,10 @@ type HandlerResult = Result<Response<Body>, Infallible>;
 async fn cv() -> HandlerResult {
     let markup =
         CvTemplate::from_str(include_str!("assets/cv.toml")).expect("Should parse cv.toml");
-    Ok(Response::new(Body::from(
-        markup.render().expect("Should render markup"),
-    )))
+    Ok(Response::builder()
+        .header(header::CONTENT_TYPE, "text/html")
+        .body(Body::from(markup.render().expect("Should render markup")))
+        .unwrap())
 }
 
 async fn four_oh_four() -> HandlerResult {
@@ -32,53 +33,69 @@ async fn index() -> HandlerResult {
     )))
 }
 
-async fn robots() -> HandlerResult {
-    Ok(Response::new(Body::from(include_str!("assets/robots.txt"))))
-}
-
 async fn image(path_str: &str) -> HandlerResult {
     let path_buf = PathBuf::from(path_str);
     let file_name = path_buf.file_name().unwrap().to_str().unwrap();
-    let ext = path_buf.extension().unwrap().to_str().unwrap();
-
-    match ext {
-        "svg" => {
-            // build the response
-            let body = {
-                let xml = match file_name {
-                    "dev-badge.svg" => include_str!("assets/images/dev-badge.svg"),
-                    "linkedin-icon.svg" => include_str!("assets/images/linkedin-icon.svg"),
-                    "github.svg" => include_str!("assets/images/github.svg"),
-                    _ => "",
+    if let Some(ext) = path_buf.extension() {
+        match ext.to_str().unwrap() {
+            "svg" => {
+                // build the response
+                let body = {
+                    let xml = match file_name {
+                        "dev-badge.svg" => include_str!("assets/images/dev-badge.svg"),
+                        "favicon.svg" => include_str!("assets/images/favicon.svg"),
+                        "linkedin-icon.svg" => include_str!("assets/images/linkedin-icon.svg"),
+                        "github.svg" => include_str!("assets/images/github.svg"),
+                        _ => "",
+                    };
+                    Body::from(xml)
                 };
-                Body::from(xml)
-            };
-            Ok(Response::builder()
+                Ok(Response::builder()
                     .status(StatusCode::OK)
                     .header(header::CONTENT_TYPE, "image/svg+xml")
                     .body(body)
-                    .unwrap(),
-            )
+                    .unwrap())
+            }
+            _ => four_oh_four().await,
         }
-        _ => four_oh_four().await,
+    } else {
+        four_oh_four().await
     }
 }
 
-pub async fn stylesheet() -> HandlerResult {
-    Ok(Response::new(Body::from(include_str!("assets/main.css"))))
+async fn string_handler(s: &str) -> HandlerResult {
+    Ok(Response::new(Body::from(s.to_string())))
+}
+
+async fn manifest() -> HandlerResult {
+    Ok(Response::builder()
+        .header(header::CONTENT_TYPE, "text/json")
+        .body(Body::from(include_str!("assets/manifest.json")))
+        .unwrap())
+}
+
+async fn stylesheet() -> HandlerResult {
+    Ok(Response::builder()
+        .header(header::CONTENT_TYPE, "text/css")
+        .body(Body::from(include_str!("assets/main.css")))
+        .unwrap())
 }
 
 pub async fn router(req: Request<Body>) -> HandlerResult {
     let (method, path) = (req.method(), req.uri().path());
     info!("{} {}", method, path);
-     match (method, path) {
-        (&Method::GET, "/") | (&Method::GET, "/index.html") => {index().await},
+    match (method, path) {
+        (&Method::GET, "/") | (&Method::GET, "/index.html") => index().await,
         (&Method::GET, "/cv") => cv().await,
         (&Method::GET, "/main.css") => stylesheet().await,
-        (&Method::GET, "/robots.txt") => robots().await,
+        (&Method::GET, "/manifest.json") => {
+            manifest().await
+        }
+        (&Method::GET, "/robots.txt") => string_handler(include_str!("assets/robots.txt")).await,
         (&Method::GET, path_str) => image(path_str).await,
         _ => {
             warn!("{}: 404!", path);
-            four_oh_four().await},
+            four_oh_four().await
+        }
     }
 }
