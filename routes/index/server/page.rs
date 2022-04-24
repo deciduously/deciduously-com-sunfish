01aed6d9721441f5397b7d13b3f59c09f63e16af
Ben Lovy <ben@deciduously.com>
2022-04-24 17:23:25 -0400

    Update, add some front page links

--- routes/index/server/page.rs
+++ routes/index/server/page.rs
@@ -1,4 +1,5 @@
 use deciduously_com_layouts::{document::Document, page_layout::PageLayout};
+use deciduously_com_ui as ui;
 use pinwheel::prelude::*;
 
 pub struct Page;
@@ -10,7 +11,26 @@ impl Component for Page {
 				PageLayout::new().child(
 					div()
 						.class("index-content")
-						.child(h1().class("index-title").child("Sunfish/Pinwheel Template")),
+						.child(h1().class("index-title").child("deciduously"))
+						.child(div().class("s1").child("Ben Lovy | Rust Developer"))
+						.child(
+							ul().child(
+								li().child("DEV.to: ").child(
+									ui::Link::new()
+										.href("https://dev.to/deciduously".to_owned())
+										.target("blank".to_owned())
+										.child("deciduously".to_owned()),
+								),
+							)
+							.child(
+								li().child("Github: ").child(
+									ui::Link::new()
+										.href("https://github.com/deciduously".to_owned())
+										.target("blank".to_owned())
+										.child("deciduously".to_owned()),
+								),
+							),
+						),
 				),
 			)
 			.into_node()
